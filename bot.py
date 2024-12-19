b644af93adb9e63b673208a3316d83382dd85e97
GodOfUranium <godofuranium@gmail.com>
2024-12-19 19:20:53 +0100

    added /help command with general and command options, added check for user in /mine

--- bot.py
+++ bot.py
@@ -13,6 +13,8 @@ TODO LIST:
   * TODO add /sell with optional item
   * TODO add /info for item info
   * TODO add /inv and /inventory to view inventory
+  * TODO add desctiptions to commands in /help
+  * TODO add desctiptions to general in /help
 """
 
 # logging
@@ -123,7 +125,7 @@ def getUser(guild:discord.Guild, linkedTo:int, getPos:bool=False):
             return user
     return None
 
-# - get item from items.json --
+# -- get item from items.json --
 def getItem(category:str, name:str, getPos:bool=False):
     with open("data/items.json") as f:
         data = json.load(f)
@@ -248,49 +250,145 @@ async def mine_command(ctx):
 
     await registerServer(ctx.guild)
 
-    # ~~~ PICK ITEM ~~~
-    # pick random category from items.json
-    with open("data/items.json") as f:
-        items = json.load(f)
-    itemCategory = items[random.choices(list(items), weights=[0.67, 0.33], k=1)[0]]
+    # check if user has Account
+    if(getUser(ctx.guild, ctx.user.id) != None):
+        # ~~~ PICK ITEM ~~~
+        # pick random category from items.json
+        with open("data/items.json") as f:
+            items = json.load(f)
+        itemCategory = items[random.choices(list(items), weights=[0.67, 0.33], k=1)[0]]
 
-    # find weights of elements
-    weights = [weight["rarity"]/100 for weight in itemCategory]
+        # find weights of elements
+        weights = [weight["rarity"]/100 for weight in itemCategory]
 
-    # choose a random item with weights
-    itemDict = random.choices(itemCategory, weights=weights, k=1)[0]
+        # choose a random item with weights
+        itemDict = random.choices(itemCategory, weights=weights, k=1)[0]
 
-    # make itemName the name of the item in the .json
-    itemName = itemDict["name"]
+        # make itemName the name of the item in the .json
+        itemName = itemDict["name"]
 
-    # ~~~ QUANTITY ~~~
-    # get inv
-    inv = getUser(ctx.guild, ctx.user.id)["inventory"]
+        # ~~~ QUANTITY ~~~
+        # get inv
+        inv = getUser(ctx.guild, ctx.user.id)["inventory"]
 
-    # add 1 to the quantity if item is existent and remove old item
-    quantity = 1
-    for item in inv:
-        if item["name"] == itemName:
-            quantity = item["quantity"] + 1
-            removeItem(ctx.guild, ctx.user.id, item)
-            break
+        # add 1 to the quantity if item is existent and remove old item
+        quantity = 1
+        for item in inv:
+            if item["name"] == itemName:
+                quantity = item["quantity"] + 1
+                removeItem(ctx.guild, ctx.user.id, item)
+                break
 
-    # ~~~ ITEM DICT ~~~
-    # create item dict
-    item = {
-        "name": itemName,
-        "quantity": quantity
-    }
+        # ~~~ ITEM DICT ~~~
+        # create item dict
+        item = {
+            "name": itemName,
+            "quantity": quantity
+        }
 
-    # add item to inv
-    addItem(ctx.guild, ctx.user.id, item)
+        # add item to inv
+        addItem(ctx.guild, ctx.user.id, item)
 
-    embed = Embed(
-        title="/mine",
-        description=f"{getUser(ctx.guild, ctx.user.id)['username']} has just mined a {item['name']}!"
-    )
+        embed = Embed(
+            title="/mine",
+            description=f"{getUser(ctx.guild, ctx.user.id)['username']} has just mined a {item['name']}!"
+        )
+    else:
+        embed = Embed(
+            title="/mine",
+            description="You can't use /mine if you don't have an account",
+            color=0xff0000
+        )
 
     await ctx.followup.send(embed=embed)
+
+# ---------- /help --------------
+@tree.command(
+    name="help",
+    description="Need any help?"
+)
+@app_commands.describe(
+    general="General help",
+    commands="Help with Commands"
+)
+@app_commands.choices(
+    general=[
+        app_commands.Choice(name="Gameplay", value="gameplay")
+    ],
+    commands=[
+        app_commands.Choice(name="Command list", value="list"),
+        app_commands.Choice(name="/register", value="register"),
+        app_commands.Choice(name="/del_account", value="del_account"),
+        app_commands.Choice(name="/mine", value="mine")
+    ]
+)
+async def help_command(ctx, 
+    general:app_commands.Choice[str]=None, 
+    commands:app_commands.Choice[str]=None
+):
+    await ctx.response.defer(ephemeral=True)
+
+    await registerServer(ctx.guild)
+
+    if general != None:
+        match general.value:
+            case "gameplay":
+                embed = Embed(
+                    title="/help general Gameplay",
+                    description="Use /mine to mine"
+                )
+
+        await ctx.followup.send(embed=embed)
+    elif commands != None:
+        match commands.value:
+            case "list":
+                embed = Embed(
+                    title="/help command list",
+                    description=
+                    "- /register\n"
+                    "  - username\n"
+                    "- /del_account\n"
+                    "- /mine"
+                )
+            case "register":
+                embed = Embed(
+                    title="/help commands /register",
+                    description=
+                    "Use /register to register yourself on this server. "
+                    "You can enter a username if you want a custom one. "
+                    "If you don't enter a custom username it will automatically be set to your discord name. "
+                )
+            case "del_account":
+                embed = Embed(
+                    title="/help commands /del_account",
+                    description=
+                    "/del_account will delete your entire account on this server. "
+                    "*Be careful*: This is *not* reversible. "
+                    "When you enter the command you will have the option to cancel the deletation by pressing \"No\". "
+                    "The \"Yes\" Button will *confirm* the deletation. "
+                )
+            case "mine":
+                embed = Embed(
+                    title="/help commands /mine",
+                    description=
+                    "Use /mine to mine a random item"
+                )
+        await ctx.followup.send(embed=embed)
+    else:
+        embed = Embed(
+            title="/help",
+            description="A basic Step by Step guide to get you started."
+        )
+        embed.add_field(
+            name="1. Register Yourself",
+            value=
+            "To get started you first need to register. "
+            "Type /register and give yourself a username if you want. "
+            "If you don't enter a username it will automatically be set to your discord name.\n"
+            "*Congratulations!* :tada:\n"
+            "You have successfully registered!"
+        )
+        await ctx.followup.send(embed=embed)
 
 ########### on_ready ###########
 @client.event
